f6801968c4f72422631e35906f254bccb06862bd
Ali Rodell <alirodell@gmail.com>
2016-07-11 15:49:16 -0400

    11 July 2016: Committing just test2.py

--- test2.py
+++ test2.py
@@ -1,10 +1,18 @@
 
-import datetime
+import boto3
+
 
 
 def main():
     
+    #aws_ses = boto3.resource('dynamodb', region_name='us-east-1', endpoint_url="http://dynamodb.us-east-1.amazonaws.com")
     
-    print(type(datetime.datetime.today()))
-
+    ses_client = boto3.client('ses', region_name='us-east-1')
+    
+    to_address= { 'ToAddresses': 'alirodell@gmail.com' }
+     
+    
+    ses_client.send_email({'Source':'screener@rodell.info','ToAddresses':'alirodell@gmail.com', 'Subject':'Test email','Body':'This is a test body'})
+            
+    
 if __name__ == "__main__": main()
